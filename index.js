471ac9e2960b891a8fe855a37ab3e6444f7e449a
LimeOrangePie <ji5489@gmail.com>
2021-05-30 16:49:58 +0900

    Fix faulty return stmt
    Signed-off-by: LimeOrangePie <ji5489@gmail.com>

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ if (process.env.NODE_ENV !== 'production') {
 /// API Body
 ///
 const router = express.Router();
-router.get('/today', async (req, res) => {
+router.get('/today', async (req, res, next) => {
     const origin = req.get('origin');
     if (origin === undefined)
         logger.info(`Successful response`);
@@ -59,28 +59,25 @@ router.get('/today', async (req, res) => {
     const mealData = await parser.fetchThisWeek();
     const currentWeekdayIdx = new Date().getDay(); // 0: Sunday ~ 6: Saturday
     if (1 <= currentWeekdayIdx <= 5) {
-        res.json({
+        return res.json({
             error: false,
             data: mealData
         });   
-    } else {
-        res.json({
-            error: "current_weekday_is_weekend",
-            data: []
-        });   
     }
-    res.send();
+    return res.json({
+        error: "current_weekday_is_weekend",
+        data: []
+    });
 });
 
 const app = express();
 app.use(router);
 app.use((req, res, next) => {
     logger.error(`Invalid request: trying to access non-existant path '${req.url}'`);
-    res.status(500).json({
+    return res.status(500).json({
         error: "invalid_request",
         data: []
     });
-    res.send();
 });
 
 const listen_port = process.env.PORT || 8080;
